47fd78db32d130b6501073286aad9908d6d275b5
tuchinski <tuchinski@alunos.utfpr.edu.br>
2018-01-31 16:12:13 -0200

    Mudança nos botões da tela Jogar

--- src/pages/jogar/jogar.ts
+++ src/pages/jogar/jogar.ts
@@ -15,7 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class JogarPage {
 
-  public Campanhas:Array<any> = [ "Leonardo", "Ilzi", "Savio" ];
+  public Campanhas:Array<any> = [ "Campanha 1", "Campanha 2", "Campanha 3", "Campanha 4", "Campanha 5", "Campanha 6", "Campanha 7" ,"Campanha 8", "Campanha 9" ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
