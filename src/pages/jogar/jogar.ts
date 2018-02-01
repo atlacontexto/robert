67b484076b350a5358c15548a95cd1cb5a068543
tuchinski <tuchinski@alunos.utfpr.edu.br>
2018-02-01 16:28:36 -0200

    Adicionado funcionalidades nos botões dos níveis na tela Jogar

--- src/pages/jogar/jogar.ts
+++ src/pages/jogar/jogar.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 /**
  * Generated class for the JogarPage page.
@@ -15,13 +17,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class JogarPage {
 
-  public Campanhas:Array<any> = [ "Campanha 1", "Campanha 2", "Campanha 3", "Campanha 4", "Campanha 5", "Campanha 6", "Campanha 7" ,"Campanha 8", "Campanha 9" ];
+  public Campanhas: Array<any> = ["Campanha 1", "Campanha 2", "Campanha 3", "Campanha 4", "Campanha 5", "Campanha 6", "Campanha 7", "Campanha 8", "Campanha 9"];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
   }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JogarPage');
   }
 
+  presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Confirme sua escolha',
+      message: 'Você escolheu a Campanha X.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Começar',
+          role: 'continuar',
+          handler: () => {
+            console.log('Continuar clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
